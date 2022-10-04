aa08e12730d51073bc230ac6413a90b83d85a170
Nickolodeon98 <sjeon0730@gmail.com>
2022-10-04 15:52:23 +0900

    Constructor and isMultiple method added

--- src/even_odd/Advanced.java
+++ src/even_odd/Advanced.java
@@ -3,6 +3,18 @@ package even_odd;
 import java.util.Scanner;
 
 public class Advanced {
+
+    private int multipleTargetNum = 2;
+
+    public Advanced(int multipleNum) {
+        this.multipleTargetNum = multipleNum;
+    }
+
+    public String isMultiple(int num) {
+        if (num % multipleTargetNum == 0) return "multiple";
+        else return "not multiple";
+    }
+
     private String discern(int num) {
         if (num % 2 == 0) return "짝수";
         else return "홀수";
@@ -18,7 +30,9 @@ public class Advanced {
         int firstNum = sc.nextInt();
         int secNum = sc.nextInt();
 
-        Advanced test = new Advanced();
+        Advanced test = new Advanced(3);
         test.printResult(firstNum, secNum);
+
+        System.out.println(test.isMultiple(245));
     }
 }
