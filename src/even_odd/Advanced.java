4bfe59e54047cb17c1f4b379d024a7ea1267a671
Nickolodeon98 <sjeon0730@gmail.com>
2022-10-04 15:29:57 +0900

    Modified class Advanced to use method to implement even and odd number checker

--- src/even_odd/Advanced.java
+++ src/even_odd/Advanced.java
@@ -3,20 +3,22 @@ package even_odd;
 import java.util.Scanner;
 
 public class Advanced {
+    private String getEvenOdd(int num) {
+        if (num % 2 == 0) return "짝수";
+        else return "홀수";
+    }
+
+    public void printEvenOddStatement(int first, int second) {
+        System.out.printf("%s+%s=%s", getEvenOdd(first), getEvenOdd(second), getEvenOdd(first + second));
+    }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int num = sc.nextInt();
+        int firstNum = sc.nextInt();
         int secNum = sc.nextInt();
-        
-        if (num % 2 == 0) System.out.print("짝수+");
-        else System.out.print("홀수+");
 
-        if (secNum % 2 == 0) System.out.print("짝수=");
-        else System.out.print("홀수=");
-
-        if ((num + secNum) % 2 == 0) System.out.print("짝수");
-        else System.out.print("홀수");
+        Advanced test = new Advanced();
+        test.printEvenOddStatement(firstNum, secNum);
     }
 }
