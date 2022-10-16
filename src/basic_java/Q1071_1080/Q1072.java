7342a6afa18641c31aae25fd38ea62a7fa456312
Nickolodeon98 <sjeon0730@gmail.com>
2022-10-16 21:55:03 +0900

    [update] 코드업 100제 1072번 풀이 완료

---
+++ src/basic_java/Q1071_1080/Q1072.java
@@ -0,0 +1,17 @@
+package basic_java.Q1071_1080;
+
+import java.util.Scanner;
+
+public class Q1072 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num;
+        num = Integer.parseInt(sc.nextLine());
+        int a;
+        for (int i = 0; i < num; i++) {
+            a = sc.nextInt();
+            System.out.println(a);
+        }
+    }
+}
